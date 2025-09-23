4c1468aba409752c6b6ddc2b0349c2cf160a25c3
Phuti Mahlaba <phutiabigail295@gmail.com>
2025-09-23 14:36:34 +0200

    removed sensetive data in db.js

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Phuti@1016",
-  database: "buildnest_db",
+ host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 const db = pool.promise();
